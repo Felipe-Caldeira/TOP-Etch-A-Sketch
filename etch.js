94734a07fd1d2abf3b6a57840861bb57d184959a
Felipe Caldeira <hms1730105@gmail.com>
2020-07-14 11:31:19 -0500

    Create basic structure and scripting for etch-a-sketch

---
+++ etch.js
@@ -0,0 +1,34 @@
+let gridSize = 16;
+
+
+
+function createGrid(size) {
+    let etchContainer = document.querySelector(".etchContainer");
+    let cellSize = Math.floor(etchContainer.offsetWidth / size);
+
+    for (i=0; i < size**2; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("etchCell");
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        //cell.addEventListener("mouseenter", fillCell);
+        cell.addEventListener("touchmove", fillCell);
+        etchContainer.appendChild(cell);
+    }
+
+
+}
+
+function fillCell(event) {
+    event.target.style.backgroundColor = "black";
+}
+
+
+
+
+
+function setup() {
+    createGrid(128);
+}
+
+window.onload = setup;
