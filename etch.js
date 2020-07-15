ea99df6eaccea950db94cd8a7cba039d091d536a
Felipe Caldeira <hms1730105@gmail.com>
2020-07-15 04:06:23 -0500

    Finish initial build of etch-a-sketch

--- etch.js
+++ etch.js
@@ -1,11 +1,12 @@
 let gridSize = 16;
+let cellColor = "black";
 
 function createGrid(size) {
     let etchContainer = document.querySelector(".etchContainer");
     let cellSize = etchContainer.offsetWidth / size;
     etchContainer.style.gridTemplateColumns = `repeat(${size}, ${cellSize}px)`
 
-    for (i=0; i < size**2; i++) {
+    for (i = 0; i < size ** 2; i++) {
         let cell = document.createElement("div");
         cell.classList.add("etchCell");
         cell.style.width = `${cellSize}px`;
@@ -19,20 +20,26 @@ function createGrid(size) {
 }
 
 function fillCell(event) {
-    let cellDarkness = event.target.getAttribute("darkness") - 0;
-    if (cellDarkness > 0) {
-        cellDarkness -= 250;
-        event.target.setAttribute("darkness", cellDarkness + "");
-        event.target.style.backgroundColor = `rgb(${cellDarkness}, ${cellDarkness}, ${cellDarkness})`;
-    }
+    event.target.style.backgroundColor = cellColor;
+    // let cellDarkness = event.target.getAttribute("darkness") - 0;
+    // if (cellDarkness > 0) {
+    //     cellDarkness -= 250;
+    //     event.target.setAttribute("darkness", cellDarkness + "");
+    //     event.target.style.backgroundColor = `rgb(${cellDarkness}, ${cellDarkness}, ${cellDarkness})`;
+    // }
 }
 
 function resetGrid() {
+    let newResolution;
+    do {
+        newResolution = prompt("Input new resolution:");
+        if (newResolution == null) return;
+    } while (newResolution == "");
+
     let oldGrid = document.querySelector(".etchContainer");
     let newClone = oldGrid.cloneNode(false);
     document.querySelector("body").replaceChild(newClone, oldGrid);
-    
-    let newResolution = prompt("Input new resolution:");
+
     createGrid(newResolution);
 }
 
@@ -40,6 +47,9 @@ function resetGrid() {
 
 function setup() {
     createGrid(16);
+    document.querySelector('input[type="color"]').addEventListener("mouseout", (e) => {
+        cellColor = document.querySelector('input[type="color"]').value;
+    })
 }
 
 window.onload = setup;
